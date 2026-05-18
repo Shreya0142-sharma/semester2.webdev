4dbf42dcf8317e0d02477d9a53b72364edeea076
Shreya0142-sharma <shreyaas.0331@gmail.com>
2026-05-18 20:48:08 +0530

    h

--- web dev/dom.js
+++ web dev/dom.js
@@ -19,3 +19,8 @@ else{
     let bodyColor=document.getElementsByTagName('body')[0];
     bodyColor.style.backgroundColor="blue";
 }
+let para=document.getElementById("paratext");
+function changetextColor(){
+    para.style.backgroundColor="#"+Math.floor(Math.random() * 16777215).toString(16);
+}
+para.addEventListener('mouseover',changetextColor);
